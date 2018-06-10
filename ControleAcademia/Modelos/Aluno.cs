7b46f47b65bc8348bc00f7384c0db214fcaf2336
RAFAEL DOHMS RISSMANN <rafariss@gmail.com>
2018-06-10 19:11:17 -0300

    Criado classe modelo

---
+++ ControleAcademia/Modelos/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Aluno
+    {
+        public int AlunoID { get; set; }
+        public string Nome { get; set; }
+        public int Matricula { get; set; }
+        public string Endereco { get; set; }
+        public DateTime DataInicio { get; set; }
+        public bool Status { get; set; }
+        Servico servico;
+
+              
+
+        //falta entender como fazer o vencimento
+
+
+    }
+}
---
+++ ControleAcademia/Modelos/Servico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    class Servico : Aluno
+    {
+        public int ValorID { get; set; }
+        public string NomeServico { get; set; }
+        public float Valor { get; set; }
+        public int ContadorDias { get; set; }
+
+
+
+    }
+}
